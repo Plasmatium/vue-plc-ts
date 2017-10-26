31a1fab7a2cb71bfc9bb5577383980e2e58ada4b
Plasmatium <slv309@163.com>
2017-10-26 22:06:18 +0800

    constructing <class SubBlock>, ownshape.inputs insertion

--- src/PLC/baseBlock.ts
+++ src/PLC/baseBlock.ts
@@ -1,19 +1,53 @@
+import * as objectPath from 'object-path'
+
 interface DPTR {
-  type: String,
-  name: String,
-  qInit?: Boolean // Output 'Q' init value
+  type: string
+  name: string
+  qInit?: boolean // Output 'Q' init value
 }
 
-interface shape {
-  subBlocks: Object,
-  interface: Object,
+interface LogicFuncParam {
+  [paramName: string]: any
 }
 
-const INPUT = class {
-  state: Boolean
+
+// Block params, not reactive. It could be reactive by other 
+// block's "Q" output.
+interface BPARAM {
+  name: string
+  linePath?: string
+  init: any
+}
+
+interface INPUT {
+  name: string
+  linePath: string
+  init?: boolean
+}
+
+interface SHAPE {
+  // 子块结构描述
+  subBlocks: DPTR[]
+
+  // 块的参数，非响应（避免不必要的响应式触发，若需要，可由罗技快触发）
+  // 参数中的linePath可提供直连subBlocks中下一层参数，如高级计数器的init直连
+  // 下一层普通计数器的init（假设高级计数器由普通计数器和其他逻辑块组成）
+  params?: BPARAM[]
+
+  // 逻辑电平输入，响应式。其linePath同上面params
+  inputs?: INPUT[]
+
+  //逻辑运算实体
+  logic: (param: LogicFuncParam) => void
+}
+
+const BaseBlock = class {} // just for type assertion
+
+const Relay = class {
+  state: boolean
   getLast: () => {
-    val: Boolean,
-    set: (newVal: Boolean) => void
+    val: boolean,
+    set: (newVal: boolean) => void
   }
   constructor (dptr: DPTR) {
     this.state = false || Boolean(dptr.qInit)
@@ -22,10 +56,10 @@ const INPUT = class {
       return {val, set: (newVal) => { val = newVal }}
     }
   }
-  toString (): Boolean {
+  toString (): boolean {
     return Boolean(this.state)
   }
-  lineIn (newState: Boolean | Number) {
+  lineIn (newState: boolean | Number) {
     newState = Boolean(newState)
     this.getLast().set(newState)
     if (newState === this.state) return
@@ -50,7 +84,39 @@ const INPUT = class {
 const RunningBlock = class {
 }
 
+const makeClosure = () => {
+  let closure = {}
+  return () => closure
+}
+
 const SubBlock = class {
-  constructor (dptr: DPTR) {
+  [propName: string]: any
+  constructor (ownShape: SHAPE) {
+    // 注入所有子块到this
+    ownShape.subBlocks.forEach(dptr => {
+      let {type, name, qInit} = dptr
+      let M = Maker[type]
+      this[name] = new M()
+    })
+
+    // 注入所有param参数至闭包
+    this.paramsClosure = makeClosure()
+    ownShape.params && ownShape.params.forEach(param => {
+      let {name, init} = param
+      // 将参数实体注入到闭包中，而不是this中（否则会被vue响应）
+      this.paramsClosure()[name] = init
+    })
+
+    // 注入所有input到this
+    // TODO:
+    ownShape.inputs && ownShape.inputs.forEach(input => {})
   }
+}
+
+// Maker
+interface IMaker {
+  [blockName: string]: any
+}
+const Maker: IMaker = {
+  Relay
 }
