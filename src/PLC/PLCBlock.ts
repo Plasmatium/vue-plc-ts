91b4e3330d3ebe027b7da09baf3801e1e3ed1acc
Plasmatium <slv309@163.com>
2017-11-29 17:01:22 +0800

    BaseBlock added

---
+++ src/PLC/PLCBlock.ts
@@ -0,0 +1,40 @@
+export class BaseBlock implements PLCBlock {
+  private static stateClosure: StateClosure = {}
+  readonly token: symbol = Symbol('token for closure')
+  Q: boolean
+  constructor (initVal?: boolean) {
+    this.Q = Boolean(initVal)
+    BaseBlock.stateClosure[this.token] = {
+      posEdge: false,
+      negEdge: false
+    }
+  }
+  toString () {
+    return this.Q
+  }
+  lineIn (newVal: boolean | number) {
+    newVal = Boolean(newVal)
+    let closure = BaseBlock.stateClosure[this.token]
+    if (newVal === this.Q) {
+      // this.Q: true -> true | false -> false
+      closure.negEdge = closure.posEdge = false
+      return
+    }
+    if (<any>this.Q - <any>newVal === 1) {
+      // this.Q: true -> false
+      closure.negEdge = true
+      closure.posEdge = false
+    } else {
+      // this.Q: false -> true
+      closure.posEdge = true
+      closure.negEdge = false
+    }
+    this.Q = newVal
+  }
+  ne () {
+    return BaseBlock.stateClosure[this.token].negEdge
+  }
+  pe () {
+    return BaseBlock.stateClosure[this.token].posEdge
+  }
+}
--- src/plc.d.ts
+++ src/plc.d.ts
@@ -3,5 +3,14 @@ interface PLCBlock {
   toString: () => boolean
   pe: () => boolean
   ne: () => boolean
-  Q?: PLCBlock
+  Q?: boolean // output of PLC Block
+  readonly token: symbol // for the entrance of closure
+}
+
+interface StateClosure {
+  // token should be a symbol
+  [token: string]: {
+    posEdge: boolean
+    negEdge: boolean
+  }
 }
